51232a437510472c74d5ca7580cf56e875f324ff
DevinAnnunzio <annunzio.da@gmail.com>
2023-10-30 17:46:44 +0900

    Feat: Reactive form with project name validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'forms-reactive-assignment';
+  theForm: FormGroup;
+  forbiddenProjectNames = ['Test', 'test']
+
+  ngOnInit(): void {
+    this.theForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, this.ForbiddenProjectNameValidator.bind(this)]),
+    })
+  }
+
+  ForbiddenProjectNameValidator(control: FormControl){
+    const value = control.value as string
+    for(let i = 0; i < this.forbiddenProjectNames.length; i++){
+      if(value === this.forbiddenProjectNames[i]){
+        return {'forbiddenProjectNames': true};
+      }
+    }
+    return null;
+  }
+
+      //Async custom validator - HAS PROBLEM
+      // ForbiddenProjectNameValidator(control: FormControl){
+      //   const value = control.value as string
+      //   const promise = new Promise<any>((resolve,reject) => {
+      //     setTimeout(() => {
+      //       for(let i = 0; i < this.forbiddenProjectNames.length; i++){
+      //         if(value === this.forbiddenProjectNames[i]){
+      //           resolve({'forbiddenProjectNames': true});
+      //         } else {
+      //           resolve(null)
+      //         }
+      //       }
+      //     },1500)
+      //   })
+  
+      //   return promise;
+      // }
+
+  onSubmit(){
+    console.log(this.theForm)
+  }
 }
